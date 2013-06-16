cac19a4819acdcd587d8907de5d1b3c7cc882ea7
Ian Underwood <ian@underwood-hq.org>
2013-06-15 20:41:40 -0400

    SQL Query fix for "listener_remove" accounting not lining up with a "listener_add" entry.

--- includes/auth.inc.php
+++ includes/auth.inc.php
@@ -24,15 +24,15 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 function uhqiceauth_checkuser($un, $pw) {
 
 	global $xoopsDB;
-	
+
 	$member_handler =& xoops_gethandler('member');
-	
+
 	// Check PW against XOOPS DB First
-	
+
 	$user =& $member_handler->loginUser($un, $pw);
-	
+
 	 if (!$user) {
-		
+
 		// Check PW against stream auth database next.
 
 		$query = "SELECT COUNT(*) FROM ".$xoopsDB->prefix('uhqiceauth_streampass');
@@ -45,7 +45,7 @@ function uhqiceauth_checkuser($un, $pw) {
 			// Query passed.  Fetch result.
 			list($matches) = $xoopsDB->fetchRow($result);
 			if ( $matches > 0 ) {
-				
+
 				// If there's a match, attempt to update the match time.
 				$query = "UPDATE ".$xoopsDB->prefix('uhqiceauth_streampass');
 				$query .= " SET used=now() WHERE un='".strtolower($un)."' AND pw='".$pw."'";
@@ -54,18 +54,18 @@ function uhqiceauth_checkuser($un, $pw) {
 				// Get the user record by name and return a uid
 				$query = "SELECT uid FROM ".$xoopsDB->prefix('users')." WHERE uname LIKE '".strtolower($un)."'";
 				$result = $xoopsDB->queryF($query);
-				
+
 				if ($result) {
 					list($uid) = $xoopsDB->fetchRow($result);
 					return($uid);
 				}
-				
+
 			}
 		}
 	} else {
 		return $user->Uid();
 	}
-	
+
 	return false;
 }
 
@@ -73,14 +73,14 @@ function uhqiceauth_checkuser($un, $pw) {
 // Returns true if the user's groups intersect with the groups passed.
 
 function uhqiceauth_checkgroup($uid, $grp) {
-	
+
 	// Get User
-	
+
 	$member_handler =& xoops_gethandler('member');
 	$user =& $member_handler->getUser($uid);
-	
+
 	// Check Group
-	
+
 	$rtn = false;
 
 	if (in_array(XOOPS_GROUP_ANONYMOUS, $grp)) {
@@ -90,19 +90,19 @@ function uhqiceauth_checkgroup($uid, $grp) {
 	}
 
 	return $rtn;
-	
+
 }
 
 // The following functions check on variables in the module configuration.
 
 function uhqiceauth_checkrdns() {
-	
+
 	// Load module options
 	$modhandler			=& xoops_gethandler('module');
 	$xoopsModule		=& $modhandler->getByDirname('uhq_iceauth');
 	$config_handler		=& xoops_gethandler('config');
 	$xoopsModuleConfig	=& $config_handler->getConfigsByCat(0,$xoopsModule->getVar('mid'));
-	
+
 	// Return true if reverse DNS is enabled in the configuration
 	if ($xoopsModuleConfig['rdns'] == 1) {
 		return true;
@@ -112,13 +112,13 @@ function uhqiceauth_checkrdns() {
 }
 
 function uhqiceauth_checklogadmin() {
-	
+
 	// Load module options
 	$modhandler			=& xoops_gethandler('module');
 	$xoopsModule		=& $modhandler->getByDirname('uhq_iceauth');
 	$config_handler		=& xoops_gethandler('config');
 	$xoopsModuleConfig	=& $config_handler->getConfigsByCat(0,$xoopsModule->getVar('mid'));
-	
+
 	// Return true if module-wide administrative update logging is enabled in the configuration
 	if ($xoopsModuleConfig['logadminupdate'] == 1) {
 		return true;
@@ -133,11 +133,11 @@ function uhqiceauth_checklogadmin() {
 function uhqiceauth_header($hdr_txt) {
 	global $iceheaders;
 	global $iceheadercount;
-	
+
 	header ($hdr_txt);
 
 	// Save added headers in an array.
-	
+
 	$iceheaders[$iceheadercount] = $hdr_txt;
 	$iceheadercount++;
 }
@@ -145,25 +145,25 @@ function uhqiceauth_header($hdr_txt) {
 // Dumps the intro file if we've got one.
 
 function uhqiceauth_introdump($mountrow=array(),$header) {
-	
+
 	global $xoopsDB;
-	
+
 	// Look and see if there are any intros.
-	
+
 	$query = "SELECT COUNT(*) FROM ".$xoopsDB->prefix("uhqiceauth_intromap");
 	$query .= " WHERE server='".$mountrow['server']."' AND";
 	$query .= " port='".$mountrow['port']."' AND";
 	$query .= " mount='".$mountrow['mount']."'";
-	
+
 	$result = $xoopsDB->queryF($query);
 	if ($result == false) {
 		$intros = 0;
 	} else {
 		list($intros) = $xoopsDB->fetchRow($result);
 	}
-	
+
 	// Set header
-	
+
 	if ($intros == 0) {
 		uhqiceauth_header($header,1);
 		return false;
@@ -222,7 +222,7 @@ function uhqiceauth_introdump($mountrow=array(),$header) {
 
 function uhqiceauth_addhit($server,$port,$mount,$hit = false) {
 	global $xoopsDB;
-	
+
 	$query = "UPDATE ".$xoopsDB->prefix('uhqiceauth_servers')." SET ";
 	if ($hit) {
 		$query .= "hits_pass = hits_pass + 1";
@@ -230,7 +230,7 @@ function uhqiceauth_addhit($server,$port,$mount,$hit = false) {
 		$query .= "hits_fail = hits_fail + 1";
 	}
 	$query .= " WHERE server = '".$server."' AND port = '".$port."' AND mount = '".$mount."'";
-	
+
 	$result = $xoopsDB->queryF($query);
 	if ($result == false) {
 		echo "<b>SQL Error at:</b> ".$query;
@@ -241,7 +241,7 @@ function uhqiceauth_addhit($server,$port,$mount,$hit = false) {
 
 function uhqiceauth_srcaddhit($server,$port,$mount,$hit = false) {
 	global $xoopsDB;
-	
+
 	$query = "UPDATE ".$xoopsDB->prefix('uhqiceauth_servers')." SET ";
 	if ($hit) {
 		$query .= "src_hits_pass = src_hits_pass + 1";
@@ -249,7 +249,7 @@ function uhqiceauth_srcaddhit($server,$port,$mount,$hit = false) {
 		$query .= "src_hits_fail = src_hits_fail + 1";
 	}
 	$query .= " WHERE server = '".$server."' AND port = '".$port."' AND mount = '".$mount."'";
-	
+
 	$result = $xoopsDB->queryF($query);
 	if ($result == false) {
 		echo "<b>SQL Error at:</b> ".$query;
@@ -260,18 +260,18 @@ function uhqiceauth_srcaddhit($server,$port,$mount,$hit = false) {
 
 function uhqiceauth_geolocate($ip) {
 	global $module_handler;
-	
+
 	$geolocate = &$module_handler->getByDirname('uhq_geolocate');
 	if (is_object($geolocate)) {
 		if ($geolocate->getVar('isactive')) {
 			require_once XOOPS_ROOT_PATH."/modules/uhq_geolocate/class/geolocate.class.php";
-			
+
 			$geoloc = new geolocate;
 			$geoloc->ipin = $ip;
 
 			$location = $geoloc->locate();
-			
-			return $location;		
+
+			return $location;
 		}
 	}
 	return NULL;
@@ -281,45 +281,45 @@ function uhqiceauth_geolocate($ip) {
 
 function uhqiceauth_authlog($sane_REQUEST, $authtype, $authstat, $authinfo=null) {
 	global $xoopsDB;
-	
+
 	// Build logging insert
 	$query = "INSERT INTO ".$xoopsDB->prefix('uhqiceauth_authtrail')." SET ";
 	$query .= "logtime = now(), ";
 	$query .= "server = '".$sane_REQUEST['server']."', ";
 	$query .= "port = '".$sane_REQUEST['port']."', ";
 	$query .= "mount = '".$sane_REQUEST['mount']."', ";
-	
+
 	// Change source authentication to A if there's an admin flag passed.
 	if (($authtype == "S") && ($_REQUEST['admin'])) {
 		// Log if admin logging is true, otherwise skip the logs altogether.
 		if (uhqiceauth_checklogadmin() == true) {
-			$query .= "authtype = 'A', ";	
+			$query .= "authtype = 'A', ";
 		} else {
 			return true;
 		}
 	} else {
 		$query .= "authtype = '".$authtype."', ";
 	}
-	
-	// If authentication passes, flag status as P, otherwise F. 
+
+	// If authentication passes, flag status as P, otherwise F.
 	if ($authstat == true) {
 		$query .= "authstat = 'P', ";
 	} else {
 		$query .= "authstat = 'F', ";
 	}
-	
+
 	// Add any comment codes if they were passed.
 	if ($authinfo != null) {
 		$query .= "authinfo = '".trim($authinfo)."', ";
 	}
-	
+
 	$query .= "clientid = '".$sane_REQUEST['client']."', ";
 	if (isset($sane_REQUEST['user'])) {
 		$query .= "username = '".$sane_REQUEST['user']."', ";
 	}
 	$query .= "useragent = '".$sane_REQUEST['agent']."', ";
 	$query .= "userip = '".$sane_REQUEST['ip']."'";
-	
+
 	// Do Geolocation
 
 	$location = uhqiceauth_geolocate($sane_REQUEST['ip']);
@@ -335,15 +335,15 @@ function uhqiceauth_authlog($sane_REQUEST, $authtype, $authstat, $authinfo=null)
 
 	// Make sure RNDS is enabled before we attempt to resolve.
 	if (uhqiceauth_checkrdns()) {
-		$query .= ", userrdns = '".gethostbyaddr($sane_REQUEST['ip'])."'";	
+		$query .= ", userrdns = '".gethostbyaddr($sane_REQUEST['ip'])."'";
 	}
-	
+
 	$result = $xoopsDB->queryF($query);
-	
+
 	if ($result == false) {
 		echo "<b>SQL Error at: </b> ".$query;
 	}
-	
+
 	// Run the hit counter
 	if ($authtype == "L") {
 		uhqiceauth_addhit($sane_REQUEST['server'], $sane_REQUEST['port'], $sane_REQUEST['mount'], $authstat);
@@ -363,14 +363,14 @@ function uhqiceauth_acctlog($sane_REQUEST) {
 	$query .= "port = '".$sane_REQUEST['port']."' AND ";
 	$query .= "mount = '".$sane_REQUEST['mount']."' AND ";
 	$query .= "clientid = '".$sane_REQUEST['client']."' AND ";
-	$query .= "duration = NULL AND ";
+	$query .= "duration IS NULL AND ";
 
 	// Limit queries to a 4-hour window.  This should cover most timezone changes.
 	$query .= "logtime > ( subtime(now(), SEC_TO_TIME(".$sane_REQUEST['duration']."+7200)) ) AND ";
 	$query .= "logtime < ( subtime(now(), SEC_TO_TIME(".$sane_REQUEST['duration']."-7200)) )";
-	
+
 	$authrecord = 0;
-	
+
 	$result = $xoopsDB->queryF($query);
 	if ($result == false) {
 		echo "<b>SQL Error at:</b> ".$query;
@@ -378,9 +378,9 @@ function uhqiceauth_acctlog($sane_REQUEST) {
 		$row = $xoopsDB->fetchArray($result);
 		if ($row['sequence']) {
 			$authrecord = $row['sequence'];
-		}	
+		}
 	}
-	
+
 	if ($authrecord) {
 		// Update authentication table, if we have infos.
 		$query = "UPDATE ".$xoopsDB->prefix('uhqiceauth_authtrail')." SET ";
@@ -407,7 +407,7 @@ function uhqiceauth_acctlog($sane_REQUEST) {
 		}
 		$query .= "useragent = '".$sane_REQUEST['agent']."', ";
 		$query .= "userip = '".$sane_REQUEST['ip']."', ";
-		
+
 		// Do Geolocation
 		$location = uhqiceauth_geolocate($sane_REQUEST['ip']);
 
@@ -419,17 +419,17 @@ function uhqiceauth_acctlog($sane_REQUEST) {
 			if ($location->city != null)
 				$query .= "geocity = '".$location->city."', ";
 		}
-		
+
 		// Make sure RNDS is enabled before we attempt to resolve.
 		if (uhqiceauth_checkrdns()) {
-			$query .= "userrdns = '".gethostbyaddr($sane_REQUEST['ip'])."', ";	
+			$query .= "userrdns = '".gethostbyaddr($sane_REQUEST['ip'])."', ";
 		}
 		$query .= "logtime = SUBTIME(now(), SEC_TO_TIME(".$sane_REQUEST['duration'].")), ";
 		$query .= "stoptime = now(), ";
 		$query .= "duration = '".$sane_REQUEST['duration']."'";
-	
+
 		$result = $xoopsDB->queryF($query);
-			
+
 		if ($result == false) {
 			echo "<b>SQL Error at:</b> ".$query;
 		}
@@ -440,7 +440,7 @@ function uhqiceauth_acctlog($sane_REQUEST) {
 
 function uhqiceauth_mountlog($sane_REQUEST) {
 	global $xoopsDB;
-	
+
 	if ($_REQUEST['action'] == 'mount_add') {
 		// Insert into Log
 		$query = "INSERT INTO ".$xoopsDB->prefix('uhqiceauth_mountlog')." SET ";
@@ -448,12 +448,12 @@ function uhqiceauth_mountlog($sane_REQUEST) {
 		$query .= "port = '".$sane_REQUEST['port']."', ";
 		$query .= "mount = '".$sane_REQUEST['mount']."', ";
 		$query .= "action = 'A'";
-		
+
 		$result = $xoopsDB->queryF($query);
 		if ($result == false) {
 			echo "<b>SQL Error at:</b> ".$query;
 		}
-		
+
 		// Set Active Mountpoint
 		$query = "INSERT IGNORE INTO ".$xoopsDB->prefix('uhqiceauth_activemounts')." SET ";
 		$query .= "server = '".$sane_REQUEST['server']."', ";
@@ -464,10 +464,10 @@ function uhqiceauth_mountlog($sane_REQUEST) {
 		if ($result == false) {
 			echo "<b>SQL Error at:</b> ".$query;
 		}
-		
+
 		return true;
 	}
-	
+
 	if ($_REQUEST['action'] == 'mount_remove') {
 		$query = "INSERT INTO ".$xoopsDB->prefix('uhqiceauth_mountlog')." SET ";
 		$query .= "server = '".$sane_REQUEST['server']."', ";
@@ -498,20 +498,20 @@ function uhqiceauth_mountlog($sane_REQUEST) {
 
 function uhqiceauth_ua_verify($testua) {
 	global $xoopsDB;
-	
+
 	return true;
-	
+
 	// Code in development for v0.9
-	
+
 	$query = "SELECT * FROM ".$xoopsDB-prefix('uhqiceauth_uabans');
-	
+
 	$result = $xoopsDB->queryF($query);
-	
+
 	if ($result == false) {
 		// If the DB fails, pass the user agent test.
 		return true;
 	}
-	
+
 	while ($row = $xoopsDB->fetchArray($result)) {
 		if (strpos($testua,$row['useragent']) === false) {
 			// UA Pases.  Do nothing.
@@ -520,7 +520,7 @@ function uhqiceauth_ua_verify($testua) {
 			return false;
 		}
 	}
-	
+
 	// Test if we pass our UA tests.
 	return true;
 }
